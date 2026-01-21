84fdec70a13560d2c80958de88d4e14211304b97
ITELVINA <itelvinasane@gmail.com>
2026-01-21 15:50:48 +0000

    Initial Angular structure for Next Step demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { OnboardingComponent } from './pages/onboarding/onboarding.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', component: HomeComponent },
+  { path: 'onboarding', component: OnboardingComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { OnboardingComponent } from './pages/onboarding/onboarding.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OnboardingComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
